1196549fdce062b0ab0dc3fbffda5806c897d40e
Killer3048 <gamert11@mail.ru>
2022-11-24 13:06:36 +0700

    main

---
+++ JS/app.js
@@ -0,0 +1,14 @@
+function del() {
+	const popup = document.getElementById("pop");
+    popup.remove();
+};
+
+function delTwo() {
+	const popupTwo = document.getElementById("pop-two");
+    popupTwo.remove();
+};
+
+function delTree() {
+	const popupTree = document.getElementById("pop-tree");
+    popupTree.remove();
+};
